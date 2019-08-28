0b661f9c6d8f108f0aab1dc307a748fb24a7d090
J.M. Thiessen <sven.thiessen@mapcreator.eu>
2019-08-28 15:37:03 +0200

    Select geometry when oceans do not intersect

--- layers/country/layer.sql
+++ layers/country/layer.sql
@@ -8,7 +8,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         NULL::int as rank
         FROM (
         -- etldoc: osm_country_polygon_gen8 -> layer_country:z0
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen8
         join lateral (
@@ -32,7 +36,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 1 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen8 -> layer_country:z2
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen8
         join lateral (
@@ -44,7 +52,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 2 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen8 -> layer_country:z3
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen8
         join lateral (
@@ -56,7 +68,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 3 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen8 -> layer_country:z4
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen8
         join lateral (
@@ -68,7 +84,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 4 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen8 -> layer_country:z5
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen8
         join lateral (
@@ -80,7 +100,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 5 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen8 -> layer_country:z6
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen8
         join lateral (
@@ -92,7 +116,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 6 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen7 -> layer_country:z7
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen7
         join lateral (
@@ -104,7 +132,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 7 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen6 -> layer_country:z8
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen6
         join lateral (
@@ -116,7 +148,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 8 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen5 -> layer_country:z9
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen5
         join lateral (
@@ -128,7 +164,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 9 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen4 -> layer_country:z10
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen4
         join lateral (
@@ -140,7 +180,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 10 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen3 -> layer_country:z11
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         -- FROM osm_country_polygon_gen3
         FROM osm_country_polygon
@@ -154,7 +198,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 11 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen2 -> layer_country:z12
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen2
         join lateral (
@@ -166,7 +214,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 12 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon_gen1 -> layer_country:z13
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon_gen1
         join lateral (
@@ -178,7 +230,11 @@ RETURNS TABLE(osm_id bigint, geometry geometry, class text, name text, name_en t
         WHERE zoom_level = 13 AND geometry && bbox
         UNION ALL
         -- etldoc: osm_country_polygon -> layer_country:z14
-        SELECT osm_id, st_difference(geometry, ocean) as geometry, 
+        SELECT osm_id,
+        case
+            when ocean is null then geometry
+            else st_difference(geometry, ocean)
+        end as geometry,
         name, name_en, name_de, tags, NULL::int as scalerank
         FROM osm_country_polygon
         join lateral (
