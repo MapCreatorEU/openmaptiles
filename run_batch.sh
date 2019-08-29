02e9e2411f61623288f4900e2b104916efc3e8ab
J.M. Thiessen <sven.thiessen@mapcreator.eu>
2019-08-29 16:02:30 +0200

    Resolve typo on heavy check mark

--- run_batch.sh
+++ run_batch.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env sh
 
+CHANNEL="notify-sven"
+
 ip=$(curl ipinfo.io/ip)
 
-./notify-slack --channel="notify-sven" --message="Started with pre-processing $ip"
+./notify-slack --channel="$CHANNEL" --message="Started with pre-processing $ip"
 ./setup_base.sh
-./notify-slack --channel="notify-sven" --message="done with pre-processing $ip"
+./notify-slack --channel="$CHANNEL" --message="done with pre-processing $ip"
 while read country
 do
     ./run_country.sh $country
     echo $country >> completed.txt
     aws s3 cp data/tiles.mbtiles s3://m4n-highlight/$country.mbtiles
-    ./notify-slack --channel="notify-sven" --message="$country uploaded :heavy_check_mark"
+    ./notify-slack --channel="$CHANNEL" --message="$country uploaded :heavy_check_mark:"
 done < countries.txt
 
-./notify-slack --channel="notify-sven" --message="Done with calculating $ip"
+./notify-slack --channel="$CHANNEL" --message="Done with calculating $ip"
