2af414a98efbfb84f6b0441a9cc073aef7e083d1
J.M. Thiessen <sven.thiessen@mapcreator.eu>
2019-08-29 13:52:16 +0200

    Add run batch script

--- run_batch.sh
+++ run_batch.sh
@@ -4,4 +4,6 @@
 while read country
 do
     ./run_country.sh $country
-done
+    echo $country >> completed.txt
+    aws s3 cp data/tiles.mbtiles s3://m4n-highlight/$country.mbtiles
+done < countries.txt
