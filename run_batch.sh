b0a45a8d954fd3b3887ba9af783b929383a2500a
J.M. Thiessen <sven.thiessen@mapcreator.eu>
2019-08-29 16:54:32 +0200

    Make sure that the file is fully read

--- run_batch.sh
+++ run_batch.sh
@@ -4,15 +4,18 @@ CHANNEL="notify-sven"
 
 ip=$(curl ipinfo.io/ip)
 
+echo "" > completed.txt
+
 ./notify-slack --channel="$CHANNEL" --message="Started with pre-processing $ip"
 ./setup_base.sh
 ./notify-slack --channel="$CHANNEL" --message="done with pre-processing $ip"
-while read country
+
+for country in $(cat countries.txt)
 do
+    echo "$country" >> completed.txt
     ./run_country.sh $country
-    echo $country >> completed.txt
     aws s3 cp data/tiles.mbtiles s3://m4n-highlight/$country.mbtiles
     ./notify-slack --channel="$CHANNEL" --message="$country uploaded :heavy_check_mark:"
-done < countries.txt
+done
 
 ./notify-slack --channel="$CHANNEL" --message="Done with calculating $ip"
