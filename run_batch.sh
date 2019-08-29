12e4941083d825512610fc35ea77be4e1bb5906e
J.M. Thiessen <sven.thiessen@mapcreator.eu>
2019-08-29 15:01:58 +0200

    Add notifications

--- run_batch.sh
+++ run_batch.sh
@@ -6,4 +6,5 @@ do
     ./run_country.sh $country
     echo $country >> completed.txt
     aws s3 cp data/tiles.mbtiles s3://m4n-highlight/$country.mbtiles
+    ./notify-send --chanell="notify-sven" --message="$country uploaded :heavy_check_mark"
 done < countries.txt
