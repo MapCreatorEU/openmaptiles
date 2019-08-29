1677a05be89f574b2f49584e3cdd621f5bae17e3
J.M. Thiessen <sven.thiessen@mapcreator.eu>
2019-08-29 15:05:30 +0200

    Change name from notify-send -> notify-slack

--- run_batch.sh
+++ run_batch.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-./notify-send --channel="notify-sven" --message="Started with pre-processing"
+./notify-slack --channel="notify-sven" --message="Started with pre-processing"
 ./setup_base.sh
-./notify-send --channel="notify-sven" --message="done with pre-processing"
+./notify-slack --channel="notify-sven" --message="done with pre-processing"
 while read country
 do
     ./run_country.sh $country
     echo $country >> completed.txt
     aws s3 cp data/tiles.mbtiles s3://m4n-highlight/$country.mbtiles
-    ./notify-send --channel="notify-sven" --message="$country uploaded :heavy_check_mark"
+    ./notify-slack --channel="notify-sven" --message="$country uploaded :heavy_check_mark"
 done < countries.txt
