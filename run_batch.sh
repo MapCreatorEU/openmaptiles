cd2cb7447d1c6fa4d0a8b63b8ace9c3144ee4ef8
J.M. Thiessen <sven.thiessen@mapcreator.eu>
2019-09-09 11:50:26 +0200

    Change run_batch commands to support yaml mapping

--- run_batch.sh
+++ run_batch.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env sh
 
-CHANNEL="notify-sven"
+readonly CHANNEL="notify-sven"
+readonly COUNTRY_FILE="country_set.yaml"
 
 ip=$(curl ipinfo.io/ip)
 
 echo "" > completed.txt
 
 ./notify-slack --channel="$CHANNEL" --message="Started with pre-processing $ip"
-./setup_base.sh
+./setup_base.sh > log 2>&1
 ./notify-slack --channel="$CHANNEL" --message="done with pre-processing $ip"
 
-for country in $(cat countries.txt)
-do
-    ./run_country.sh $country
-    aws s3 cp data/tiles.mbtiles s3://m4n-highlight/$country.mbtiles
+readonly count=$(cat $COUNTRY_FILE  | yq .code | wc -l)
+for i in `seq 0 $count`; do
+    country=$(cat $COUNTRY_FILE | yq .[$i].name)
+    iso_code=$(cat $COUNTRY_FILE | yq .[$i].code)
+    iso_code=$(echo $iso_code | tr -d \")
+    country=$(echo $country | tr -d \")
+
+    sed -i '/^ISO_CODE/d' .env
+    echo "ISO_CODE=$iso_code" >> .env
+
+    ./run_country.sh $country >> log 2>&1
+    aws s3 cp data/tiles.mbtiles s3://m4n-highlight/$country.mbtiles >> log 2>&1
     ./notify-slack --channel="$CHANNEL" --message="$country uploaded :heavy_check_mark:"
     echo "$country" >> completed.txt
 done
