a02db747859d36fe723f42e54b0120bd4ca37997
J.M. Thiessen <sven.thiessen@mapcreator.eu>
2019-09-09 13:17:12 +0200

    Do not import osm data

--- run_country.sh
+++ run_country.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 rm -rf ./data/*
 make download-geofabrik area=$1
-docker-compose run --rm import-osm
+#docker-compose run --rm import-osm
 docker-compose run --rm import-sql
 make psql-analyze
 docker-compose run --rm import-countries
